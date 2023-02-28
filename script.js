cdc6897c80c388fc24af94eec9320d87d66ba41e
dalailahner <59581086+dalailahner@users.noreply.github.com>
2023-02-28 11:22:52 +0100

    fixed description fill function

--- script.js
+++ script.js
@@ -29,12 +29,42 @@ function checkTime(x) { // add zero in front of single digits
 }
 
 // XML FEED
-fetch('https://www.salzburg24.at/newsletter/busdisplay/busxml/xml')
-  .then((response) => response.text())
-  .then((data) => {
-    const parser = new DOMParser();
-    const xml = parser.parseFromString(data, 'application/xml');
-    let items = xml.getElementsByTagName('item');
-    document.getElementById('headline').innerHTML = items[0].getElementsByTagName('title')[0].childNodes[0].nodeValue;
-  })
-  .catch(console.error);
+function getXML() {
+  fetch('https://www.salzburg24.at/newsletter/busdisplay/busxml/xml')
+    .then((response) => response.text())
+    .then((data) => {
+      let descr1 = document.querySelectorAll('.description')[0];
+      let descr2 = document.querySelectorAll('.description')[1];
+      const parser = new DOMParser();
+      const xml = parser.parseFromString(data, 'application/xml');
+      let items = xml.getElementsByTagName('item');
+      // HEADLINE
+      document.getElementById('headline').innerHTML = items[0].getElementsByTagName('title')[0].childNodes[0].nodeValue;
+      // DESCRIPTION
+      let descriptionTxt = items[0].getElementsByTagName('description')[0].childNodes[0].nodeValue.split(' ');
+      let txtForDescr2 = fillDescriptionAndReturnRest(descriptionTxt, descr1);
+      console.log(txtForDescr2);
+      let remainingTxtFromDescr2 = fillDescriptionAndReturnRest(txtForDescr2, descr2);
+      console.log(remainingTxtFromDescr2);
+    })
+    .catch(console.error);
+}
+
+function fillDescriptionAndReturnRest(txtToFill, elToFill) {
+  elToFill.innerHTML = txtToFill.join(" ");
+  elToFill.style.height = window.getComputedStyle(elToFill).height;
+  let baseLineHeight = parseFloat(window.getComputedStyle(elToFill).lineHeight);
+  let newHeight = parseFloat(elToFill.clientHeight / baseLineHeight * baseLineHeight);
+  elToFill.style.height = "fit-content";
+  for (let index = txtToFill.length; index > 1; index--) {
+    let fillTxt = txtToFill.slice(0, index);
+    elToFill.innerHTML = fillTxt.join(" ");
+    console.log(elToFill.scrollHeight + " <? " + newHeight);
+    if (elToFill.scrollHeight < newHeight) {
+      let remainingTxt = txtToFill.slice(index, txtToFill.length);
+      elToFill.style.overflow = "visible";
+      console.log("DONE");
+      return remainingTxt;
+    }
+  }
+}
