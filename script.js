088958d413d2cefac103ebce8d0e287ae75e4759
dalailahner <59581086+dalailahner@users.noreply.github.com>
2023-03-06 17:52:31 +0100

    added loop function

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+// GLOBAL VARIABLES
+var items;
+var xmlIndex = 0;
+var loopDuration = 3000;
+
+getXML();
+
 // DATUM
 function getDate() {
   let jsDate = new Date();
@@ -33,36 +40,51 @@ function getXML() {
   fetch('https://www.salzburg24.at/newsletter/busdisplay/busxml/xml')
     .then((response) => response.text())
     .then((data) => {
-      let descr1 = document.querySelectorAll('.description')[0];
-      let descr2 = document.querySelectorAll('.description')[1];
       const parser = new DOMParser();
       const xml = parser.parseFromString(data, 'application/xml');
-      let items = xml.getElementsByTagName('item');
-      // HEADLINE
-      document.getElementById('headline').innerHTML = items[0].getElementsByTagName('title')[0].childNodes[0].nodeValue;
-      // DESCRIPTION
-      let descriptionTxt = items[0].getElementsByTagName('description')[0].childNodes[0].nodeValue.split(' ');
-      let txtForDescr2 = fillDescriptionAndReturnRest(descriptionTxt, descr1);
-      console.log(txtForDescr2);
-      let remainingTxtFromDescr2 = fillDescriptionAndReturnRest(txtForDescr2, descr2);
-      console.log(remainingTxtFromDescr2);
-      // QR CODE
-      let articleURL = items[0].getElementsByTagName('link')[0].childNodes[0].nodeValue;
-      let qrcode = new QRCode(document.getElementById('qrcode'), {
-        text: articleURL,
-        width: 500,
-        height: 500,
-        colorDark: "#000000",
-        colorLight: "#ffffff",
-        correctLevel: QRCode.CorrectLevel.L
-      });
-      // IMAGE
-      let articleImg = document.getElementById('articleIMG');
-      let articleImgUrl = items[0].getElementsByTagName('enclosure')[0].getAttribute('url');
-      console.log(articleImgUrl);
-      articleImg.style.backgroundImage = "url(" + articleImgUrl + ")";
+      items = xml.getElementsByTagName('item');
+      pasteArticle();
+      loopArticles();
     })
     .catch(console.error);
+}
+
+function loopArticles() {
+  setTimeout(() => {
+    pasteArticle();
+    loopArticles();
+  }, loopDuration);
+}
+
+function pasteArticle() {
+  let headline = document.getElementById('headline');
+  let descr1 = document.querySelectorAll('.description')[0];
+  let descr2 = document.querySelectorAll('.description')[1];
+  // HEADLINE
+  headline.innerHTML = items[xmlIndex].getElementsByTagName('title')[0].childNodes[0].nodeValue;
+  // DESCRIPTION
+  let descriptionTxt = items[xmlIndex].getElementsByTagName('description')[0].childNodes[0].nodeValue.split(' ');
+  let txtForDescr2 = fillDescriptionAndReturnRest(descriptionTxt, descr1);
+  let remainingTxtFromDescr2 = fillDescriptionAndReturnRest(txtForDescr2, descr2);
+  // QR CODE
+  // TODO: FIX QR CODE GENERATOR
+  let articleURL = items[xmlIndex].getElementsByTagName('link')[0].childNodes[0].nodeValue;
+  let qrcode = new QRCode(document.getElementById('qrcode'), {
+    text: articleURL,
+    width: 500,
+    height: 500,
+    colorDark: "#000000",
+    colorLight: "#ffffff",
+    correctLevel: QRCode.CorrectLevel.L
+  });
+  // IMAGE
+  let articleImg = document.getElementById('articleIMG');
+  let articleImgUrl = items[xmlIndex].getElementsByTagName('enclosure')[0].getAttribute('url');
+  articleImg.style.backgroundImage = "url(" + articleImgUrl + ")";
+  xmlIndex++;
+  if (xmlIndex == items.length) {
+    xmlIndex = 0;
+  }
 }
 
 function fillDescriptionAndReturnRest(txtToFill, elToFill) {
@@ -74,11 +96,9 @@ function fillDescriptionAndReturnRest(txtToFill, elToFill) {
   for (let index = txtToFill.length; index > 1; index--) {
     let fillTxt = txtToFill.slice(0, index);
     elToFill.innerHTML = fillTxt.join(" ");
-    console.log(elToFill.scrollHeight + " <? " + newHeight);
     if (elToFill.scrollHeight < newHeight) {
       let remainingTxt = txtToFill.slice(index, txtToFill.length);
       elToFill.style.overflow = "visible";
-      console.log("DONE");
       return remainingTxt;
     }
   }
